fa57d5e16a5115f83b4d4df02829257268c501d3
Fer-Barrera2005 <20220023@santacecilia.edu.sv>
2022-05-06 17:40:28 -0500

    Proyecto Terminado

--- ProyectoPTC/Form2.cs
+++ ProyectoPTC/Form2.cs
@@ -66,49 +66,49 @@ namespace ProyectoPTC
             string numero5 = textBox5.Text;
             int n5 = Convert.ToInt32(numero5);
 
-            //if (n1 >= n2 && n1 >= n3 && n1 >= n4 && n1 >= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n1} es el número mayor");
-            //}
-            //else if (n2 >= n1 && n2 >= n3 && n2 >= n4 && n2 >= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n2} es el número mayor");
-            //}
-            //else if (n3 >= n1 && n3 >= n2 && n3 >= n4 && n3 >= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n3} es el número mayor");
-            //}
-            //else if (n4 >= n1 && n4 >= n2 && n4 >= n3 && n4 >= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n4} es el número mayor");
-            //}
-            //else if (n5 >= n1 && n5 >= n2 && n5 >= n3 && n5 >= n4)
-            //{
-            //    MessageBox.Show($"La cantidad {n5} es el número mayor");
-            //}
+            if (n1 >= n2 && n1 >= n3 && n1 >= n4 && n1 >= n5)
+            {
+                MessageBox.Show($"La cantidad {n1} es el número mayor");
+            }
+            else if (n2 >= n1 && n2 >= n3 && n2 >= n4 && n2 >= n5)
+            {
+                MessageBox.Show($"La cantidad {n2} es el número mayor");
+            }
+            else if (n3 >= n1 && n3 >= n2 && n3 >= n4 && n3 >= n5)
+            {
+                MessageBox.Show($"La cantidad {n3} es el número mayor");
+            }
+            else if (n4 >= n1 && n4 >= n2 && n4 >= n3 && n4 >= n5)
+            {
+                MessageBox.Show($"La cantidad {n4} es el número mayor");
+            }
+            else if (n5 >= n1 && n5 >= n2 && n5 >= n3 && n5 >= n4)
+            {
+                MessageBox.Show($"La cantidad {n5} es el número mayor");
+            }
 
 
 
-            ////if (n1 <= n2 && n1 <= n3 && n1 <= n4 && n1 <= n5)
-            ////{
-            //    MessageBox.Show($"La cantidad {n1} es el número mayor");
-            //}
-            //else if (n2 <= n1 && n2 <= n3 && n2 <= n4 && n2 <= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n2} es el número mayor");
-            //}
-            //else if (n3 <= n1 && n3 <= n2 && n3 <= n4 && n3 <= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n3} es el número mayor");
-            //}
-            //else if (n4 <= n1 && n4 <= n2 && n4 <= n3 && n4 <= n5)
-            //{
-            //    MessageBox.Show($"La cantidad {n4} es el número mayor");
-            //}
-            //else if (n5 <= n1 && n5 <= n2 && n5 <= n3 && n5 <= n4)
-            //{
-            //    MessageBox.Show($"La cantidad {n5} es el número mayor");
-            //}
+            if (n1 <= n2 && n1 <= n3 && n1 <= n4 && n1 <= n5)
+            {
+                MessageBox.Show($"La cantidad {n1} es el número mayor");
+        }
+            else if (n2 <= n1 && n2 <= n3 && n2 <= n4 && n2 <= n5)
+            {
+                MessageBox.Show($"La cantidad {n2} es el número mayor");
+            }
+            else if (n3 <= n1 && n3 <= n2 && n3 <= n4 && n3 <= n5)
+            {
+                MessageBox.Show($"La cantidad {n3} es el número mayor");
+            }
+            else if (n4 <= n1 && n4 <= n2 && n4 <= n3 && n4 <= n5)
+            {
+                MessageBox.Show($"La cantidad {n4} es el número mayor");
+            }
+            else if (n5 <= n1 && n5 <= n2 && n5 <= n3 && n5 <= n4)
+            {
+                MessageBox.Show($"La cantidad {n5} es el número mayor");
+            }
         }
     }
 }

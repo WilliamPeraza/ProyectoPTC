17452487e7c2f9ee4a6edcbcb5c65f727fa824f2
F7e4 <20130072@santacecilia.edu.sv>
2022-05-06 12:39:35 -0500

    Proyecto 3

--- ProyectoPTC/Form3.cs
+++ ProyectoPTC/Form3.cs
@@ -16,5 +16,25 @@ namespace ProyectoPTC
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form4 Menu = new Form4();
+            Menu.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Su Prestamo Fue de 1,000,000$" +
+                " Por su 1 año del 27% de interes el total es: 270,000$ " + " Su deuda Total seria de: 1,270,000$");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Su Prestamo Fue de 1,000,000$" +
+                " Por su 2 año del 27% de interes el total es: 540,000$ " + " Su deuda Total seria de: 1,540,000$");
+        }
     }
 }
